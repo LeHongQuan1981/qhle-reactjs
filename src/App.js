d634c45ee98bfaf777d424e736988f4b409dcc44
Quan Le <qhle1981@gmail.com>
2024-09-24 18:02:00 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          qhle1981@gmail.com
         </p>
         <a
           className="App-link"
